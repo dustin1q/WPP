5e025eb69ca1d3d71a02c89497935bc2c1ba0f70
dustin1q <dustin@softsights.io>
2024-09-18 10:30:26 -0400

    update grid and DNS

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tunnel from "astro-tunnel";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dustin1q.github.io',
-  base: '/WPP',
+  base: '/',
   integrations: [react({
     experimentalReactChildren: true
   }), tunnel()],

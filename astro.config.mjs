b8d5c951c4d6d57e54606e00dd489d448ef05217
dustin1q <dustin@softsights.io>
2025-09-17 12:46:55 -0400

    update astro to version 5.13.8

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,14 @@ import { defineConfig } from 'astro/config';
 import remarkToc from 'remark-toc';
 import react from "@astrojs/react";
 
-import tunnel from "astro-tunnel";
-
 // https://astro.build/config
 export default defineConfig({
-  
   site: 'https://dustin1q.github.io',
   base: '/',
-  integrations: [react({
-    experimentalReactChildren: true
-  }), tunnel()],
-  renderers: ['@astrojs/renderer-react'],
+  integrations: [
+    react()
+  ],
   markdown: {
-    // Applied to .md and .mdx files
     remarkPlugins: [remarkToc],
-  }
+  },
 });

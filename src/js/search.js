0f1365d81a6415a06c052b4ea1ec49bbd3ae25f9
dustin1q <dustin@softsights.io>
2026-03-05 15:40:09 -0500

    update search and search results

--- src/js/search.js
+++ src/js/search.js
@@ -2,7 +2,7 @@ import DOMPurify from "dompurify";
 import Fuse from "fuse.js";
 let SEARCH_DATA;
 let FUSE_INSTANCE;
-const FUSE_OPTIONS ={
+const FUSE_OPTIONS = {
     includeScore: true,
     shouldSort: true,
     threshold: 0.3,
@@ -13,11 +13,11 @@ const FUSE_OPTIONS ={
             weight: 1,
         },
         {
-            name:"desc",
+            name: "desc",
             weight: 0.75,
         },
     ],
-   
+
 }
 const SPINNER = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256" id="spinner"><path d="M232,128a104,104,0,0,1-208,0c0-41,23.81-78.36,60.66-95.27a8,8,0,0,1,6.68,14.54C60.15,61.59,40,93.27,40,128a88,88,0,0,0,176,0c0-34.73-20.15-66.41-51.34-80.73a8,8,0,0,1,6.68-14.54C208.19,49.64,232,87,232,128Z"></path>
     <style>
@@ -35,62 +35,62 @@ const searchReadout = document.querySelector("#searchReadout");
 const searchNumber = document.querySelector("#searchNumber");
 const resultsList = document.querySelector("#resultsList");
 
-function updateDocumentTitle(search){
-    document.title = search ? `Search results for "${search}"`:`Search`
+function updateDocumentTitle(search) {
+    document.title = search ? `Search results for "${search}"` : `Search`
 }
-function updateSearchNumber(resultsNumber){
-    searchNumber.textContent = resultsNumber > 0 ? ` ${resultsNumber} Found`:``
- }
-function updateSearchReadout(search){
-   searchReadout.textContent = search ? `Search on "${search}". `:``
+function updateSearchNumber(resultsNumber) {
+    searchNumber.textContent = resultsNumber > 0 ? ` ${resultsNumber} Found` : ``
 }
-function updateSearchPageURL(search){
+function updateSearchReadout(search) {
+    searchReadout.textContent = search ? `Search on "${search}". ` : ``
+}
+function updateSearchPageURL(search) {
     const url = new URL(window.location.href);
     url.searchParams.set("q", search);
     window.history.replaceState(null, "", url);
 }
 
-function generateSearchList(results){
-    
-    if(results.length == 0){
-       
+function generateSearchList(results) {
+
+    if (results.length == 0) {
+
         return;
-    } 
-    
+    }
+
     return results.map((r) => {
         //console.log(r);
-        const {title, desc, slug, cat} = r.item;
+        const { title, desc, slug, cat } = r.item;
         return `<li>
-                    <textarea disabled="true" style="border:none;resize: none; overflow:hidden;">${desc}</textarea>
+                    <textarea disabled="true">${desc}</textarea>
                     <a href="/${cat}/${slug}">${title}</a>
                 </li>`;
     }).join("");
 }
-async function fetchSearchResults(search){
-    if(search.length === 0){
+async function fetchSearchResults(search) {
+    if (search.length === 0) {
         resultsList.innerHTML = "";
         updateSearchNumber(0);
         return;
     }
     resultsList.innerHTML = SPINNER;
-    if(!SEARCH_DATA){
-        try{
+    if (!SEARCH_DATA) {
+        try {
             const res = await fetch("/search.json");
-            if(!res.ok){throw new Error("something is wrong???")}
+            if (!res.ok) { throw new Error("something is wrong???") }
             const data = await res.json();
-            SEARCH_DATA =  data;
-        } catch (e){
+            SEARCH_DATA = data;
+        } catch (e) {
             console.error(e);
         }
     }
-   
-    if(SEARCH_DATA && !FUSE_INSTANCE){
+
+    if (SEARCH_DATA && !FUSE_INSTANCE) {
         //console.log("generting new fuse")
         FUSE_INSTANCE = new Fuse(SEARCH_DATA, FUSE_OPTIONS);
     }
-    if(!FUSE_INSTANCE)return;
+    if (!FUSE_INSTANCE) return;
     //console.log("before fuse: "+search.length)
-    const searchResult = FUSE_INSTANCE.search(search); 
+    const searchResult = FUSE_INSTANCE.search(search);
     resultsList.innerHTML = searchResult.length > 0 ? generateSearchList(searchResult) : "No samples found…";
     updateSearchNumber(searchResult.length);
     //console.log(searchResult)
@@ -100,14 +100,14 @@ window.addEventListener("DOMContentLoaded", () => {
     const urlParams = DOMPurify.sanitize(new URLSearchParams(window.location.search).get("q"));
     fetchSearchResults(urlParams);
     updateDocumentTitle(urlParams);
-    updateSearchReadout(urlParams); 
+    updateSearchReadout(urlParams);
     search.value = urlParams;
     search.focus();
 });
 window.addEventListener("input", () => {
     const searchTerm = DOMPurify.sanitize(search.value);
     updateDocumentTitle(searchTerm);
-    updateSearchReadout(searchTerm); 
+    updateSearchReadout(searchTerm);
     fetchSearchResults(searchTerm);
     updateSearchPageURL(searchTerm);
 });
